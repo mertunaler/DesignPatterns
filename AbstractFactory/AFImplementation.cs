a828bb66fe3b25087394d32757157fba4b830654
Mert Unaler <Mert.Unaler@logo.com.tr>
2023-10-06 22:30:19 +0300

    +AbstractFactory added.

---
+++ AbstractFactory/AFImplementation.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public static class AFImplementation
+    {
+        public interface IVan
+        {
+            void Assemble();
+        }
+
+        public interface IAutomobile
+        {
+            void Assemble();
+        }
+
+        public interface AbstractFactory
+        {
+            public IVan ProduceVan();
+            public IAutomobile ProduceAutomobile();
+
+        }
+
+        public class CitroenVan : IVan
+        {
+            public void Assemble()
+            {
+                Console.WriteLine("Putting Citroen Amblems...");
+                Console.WriteLine("DONE!!");
+            }
+        }
+
+        public class FiatVan : IVan
+        {
+            public void Assemble()
+            {
+                Console.WriteLine("Putting Fiat Amblems...");
+                Console.WriteLine("DONE!!");
+            }
+        }
+
+        public class CitroenAutomobile : IAutomobile
+        {
+            public void Assemble()
+            {
+                Console.WriteLine("Putting Citroen Amblems...");
+                Console.WriteLine("DONE!!");
+            }
+        }
+
+        public class FiatAutomobile : IAutomobile
+        {
+            public void Assemble()
+            {
+                Console.WriteLine("Putting Citroen Amblems...");
+                Console.WriteLine("DONE!!");
+            }
+        }
+
+
+
+        public class CitroenFactory : AbstractFactory
+        {
+            public IAutomobile ProduceAutomobile()
+            {
+                return new CitroenAutomobile();
+            }
+
+            public IVan ProduceVan()
+            {
+                return new CitroenVan();
+            }
+        }
+
+        public class FiatFactory : AbstractFactory
+        {
+            public IAutomobile ProduceAutomobile()
+            {
+                return new FiatAutomobile();
+            }
+
+            public IVan ProduceVan()
+            {
+                return new FiatVan();
+            }
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,20 @@
+﻿using AbstractFactory;
+
+public class Program
+{
+    static void Main()
+    {
+        AFImplementation.AbstractFactory _Cfactory = new AFImplementation.CitroenFactory();
+        AFImplementation.AbstractFactory _Ffactory = new AFImplementation.FiatFactory();
+
+
+        _Ffactory.ProduceVan().Assemble();
+
+
+        _Cfactory.ProduceVan().Assemble();
+
+    }
+}
+
+
+
