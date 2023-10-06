75cd44522f965c2e319d7e367a7c0908d06f28b5
Mert Unaler <Mert.Unaler@logo.com.tr>
2023-10-06 21:13:15 +0300

    Add project files.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,61 @@
+﻿using MongoDB.Driver;
+using System.Data.SqlClient;
+
+public interface IConnection
+{
+    object GetConnection();
+}
+
+public class MsSqlConnection : IConnection
+{
+    public object GetConnection()
+    {
+        return new SqlConnection("CONNSTRR");
+    }
+}
+
+public class MongoDbConnection : IConnection
+{
+    public object GetConnection()
+    {
+        return new MongoClient("mongodb://localhost:1923");
+    }
+}
+
+public abstract class ConnectionFactory
+{
+    public abstract IConnection CreateConnection();
+}
+
+public class MongoDbFactory : ConnectionFactory
+{
+    public override IConnection CreateConnection()
+    {
+        return new MongoDbConnection(); 
+    }
+}
+
+public class MsSqlFactory : ConnectionFactory
+{
+    public override IConnection CreateConnection()
+    {
+        return new MsSqlConnection();
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        ConnectionFactory _conn = new MsSqlFactory();
+        
+        _conn.CreateConnection();
+
+        ConnectionFactory _mongoConn = new MongoDbFactory();
+        _mongoConn.CreateConnection();
+    }
+}
+
+
+
+
